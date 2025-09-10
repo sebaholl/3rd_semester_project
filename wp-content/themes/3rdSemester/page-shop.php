61a58169d1cad23b6308c5b9d72cd40f473a3c43
Clifbn <cliben01@easv365.dk>
2025-09-10 15:01:38 +0200

    html and css setup

---
+++ wp-content/themes/3rdSemester/page-shop.php
@@ -0,0 +1,25 @@
+
+<?php get_header(); ?>
+
+
+
+
+<div class="product-container">
+  <!-- Product Card -->
+  <div class="product-card">
+    <div class="product-image">
+      <img src="/wp-content/themes/3rdSemester/pictures/runningShoe.png" alt="ASICS Gel-Kayano 32"> 
+    </div>
+    <div class="product-info">
+      <h3>ASICS Gel-Kayano 32 Løbesko Herre</h3>
+      <div class="price">
+        <span class="current">1.500 kr.</span>
+        <span class="old">1.700 kr.</span>
+      </div>
+      <div class="member-price">BUY NOW</div>
+    </div>
+  </div>
+
+
+
+<?php get_footer(); ?>
